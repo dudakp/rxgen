2243f41a1b63d835d4f7a37243c0b995c0475c52
dudakp <M2jt6vSDS>
2019-09-08 16:42:07 +0200

    nothing

--- actionCreators.js
+++ actionCreators.js
@@ -1,44 +1,12 @@
 import {
-    AHOJ_CAV,FETCH_OK,OK,M
+    AHOJ,FERO
 } from "./actionTypes";
 
-export const ahojCav = payload => ({
-  type: AHOJ_CAV,
+export const ahoj = payload => ({
+  type: AHOJ,
   payload
 });
-export const  fetchOk = payload => ({
-  type: FETCH_OK,
-  payload
-});
-export const  ok = payload => ({
-  type: OK,
-  payload
-});
-export const  m = payload => ({
-  type: M,
-  payload
-});
-import {
-    DOBRE_CAV
-} from "./actionTypes";
-
-export const DOBRECAV = payload => ({
-  type: DOBRECAV,
-  payload
-});
-import {
-    M_N
-} from "./actionTypes";
-
-export const mN = payload => ({
-  type: M_N,
-  payload
-});
-import {
-    KRASNE_FUNGUJEME
-} from "./actionTypes";
-
-export const krasneFungujeme = payload => ({
-  type: KRASNE_FUNGUJEME,
+export const  fero = payload => ({
+  type: FERO,
   payload
 });
